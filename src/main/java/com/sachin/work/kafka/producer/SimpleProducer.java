9a967f3fd72d2d2e30e7467b85de7efe440d6d3a
Sachin Shukla <sachin.shukla@deskera.com>
2020-12-26 19:34:06 +0530

    Producer Changes

--- src/main/java/com/sachin/work/kafka/producer/SimpleProducer.java
+++ src/main/java/com/sachin/work/kafka/producer/SimpleProducer.java
@@ -1,10 +1,14 @@
 package com.sachin.work.kafka.producer;
 
+import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
+import org.springframework.util.concurrent.ListenableFuture;
 
 @Service
 public class SimpleProducer {
@@ -16,9 +20,14 @@ public class SimpleProducer {
   @Autowired
   private KafkaTemplate<String, String> kafkaTemplate;
 
-  public void produce(final String msg) {
-    final ProducerRecord<String, String> record = new ProducerRecord<>("mytopic", null, msg);
-    this.kafkaTemplate.send(record);
+  public ListenableFuture<SendResult<String, String>> produce(final String msg) {
+    final ProducerRecord<String, String> record = new ProducerRecord<>(TOPIC, null, msg);
+    return this.kafkaTemplate.send(record);
+  }
+
+  public ListenableFuture<SendResult<String, String>> produce(final String key, final String msgVal) {
+    final ProducerRecord<String, String> record = new ProducerRecord<>(TOPIC, key, msgVal);
+    return this.kafkaTemplate.send(record);
   }
 
 }
--- src/test/java/com/sachin/work/kafka/producer/ProducerTest.java
+++ src/test/java/com/sachin/work/kafka/producer/ProducerTest.java
@@ -1,8 +1,12 @@
 package com.sachin.work.kafka.producer;
 
 import com.sachin.work.kafka.BaseTest;
+import java.util.concurrent.ExecutionException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
 
 
 class ProducerTest extends BaseTest {
@@ -11,7 +15,51 @@ class ProducerTest extends BaseTest {
   private SimpleProducer simpleProducer;
 
   @Test
-  void testProduceMessage() {
-    this.simpleProducer.produce("ABCD2");
+  void produceMessage_FireAndForget() throws InterruptedException, ExecutionException {
+    this.simpleProducer.produce("Message produced from Java Layer");
+  }
+
+
+  @Test
+  void sendMessageToDifferentPartitions() throws InterruptedException, ExecutionException {
+    push("1", "Message {1} produced from Java Layer, Sync");
+    push("2", "Message {2} produced from Java Layer, Sync");
+    push("3", "Message {3} produced from Java Layer, Sync");
+    push("4", "Message {4} produced from Java Layer, Sync");
+    push("5", "Message {5} produced from Java Layer, Sync");
+  }
+
+  private void push(final String key, final String msgVal) throws InterruptedException, ExecutionException {
+    final ListenableFuture<SendResult<String, String>> future =  this.simpleProducer.produce(key, msgVal);
+    final SendResult<String, String> result = future.get();
+    System.out.println(String.format("Message pushed to partition {%d} with offset as {%d}", result.getRecordMetadata().partition(), result.getRecordMetadata().offset()));
+  }
+
+  @Test
+  void produceMessage_Sync() throws InterruptedException, ExecutionException {
+    final ListenableFuture<SendResult<String, String>> future =  this.simpleProducer.produce("Message produced from Java Layer, Sync");
+    final SendResult<String, String> result = future.get();
+    System.out.println("Metadata : " + result.getRecordMetadata().offset());
+    System.out.println("Execution Done for method produceMessage_Sync()");
+  }
+
+  @Test
+  void produceMessage_ASync() throws InterruptedException, ExecutionException {
+    final ListenableFuture<SendResult<String, String>> future =  this.simpleProducer.produce("Message produced from Java Layer, Async");
+    future.addCallback( new ListenableFutureCallback<SendResult<String, String>>() {
+
+      @Override
+      public void onSuccess(final SendResult<String, String> result) {
+        System.out.println(String.format("Sent the message successfully with offset {%d} .", result.getRecordMetadata().offset()));
+      }
+
+      @Override
+      public void onFailure(final Throwable ex) {
+        System.out.println(String.format("Exception while sending message {%s}", ex.getMessage()));
+      }
+    });
+
+    System.out.println("Execution Done for method produceMessage_ASync()");
+    Thread.currentThread().sleep(1000);
   }
 }
