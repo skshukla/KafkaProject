9755c2f770831bd4d9e6c3e9cacfb3c651b84511
Sachin Shukla <sachin.shukla@deskera.com>
2020-12-27 17:48:00 +0530

    Supporting Avro Serialization

--- src/main/java/com/sachin/work/kafka/config/KafkaProjectConfig.java
+++ src/main/java/com/sachin/work/kafka/config/KafkaProjectConfig.java
@@ -1,6 +1,8 @@
 package com.sachin.work.kafka.config;
 
-import com.sachin.work.kafka.producer.CustomPartitioner;
+import com.google.common.collect.ImmutableMap;
+import com.sachin.work.kafka.vo.Employee;
+import io.confluent.kafka.serializers.KafkaAvroSerializer;
 import java.util.HashMap;
 import java.util.Map;
 import org.apache.kafka.clients.producer.ProducerConfig;
@@ -19,23 +21,38 @@ public class KafkaProjectConfig {
   private String KAFKA_BROKERS;
 
   @Bean
-  public KafkaTemplate<String, String> kafkaTemplate(){
+  public KafkaTemplate<String, String> kafkaTemplate() {
     final KafkaTemplate<String, String> kafkaTemplate = new KafkaTemplate<>(this.producerFactory());
     return kafkaTemplate;
   }
 
-  public ProducerFactory<String, String> producerFactory() {
+  @Bean
+  public KafkaTemplate<String, Employee> kafkaTemplate_AvroSerializerAndDeserializer() {
+    final KafkaTemplate<String, Employee> kafkaTemplate = new KafkaTemplate<>(this.producerFactory_AvroSerializerAndDeserializer());
+    return kafkaTemplate;
+  }
+
+
+  private ProducerFactory<String, String> producerFactory() {
     final Map<String, Object> props = new HashMap<>();
     props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, KAFKA_BROKERS);
     props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
     props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
 
 //    props.put(ProducerConfig.PARTITIONER_CLASS_CONFIG, CustomPartitioner.class);
-
-
     props.put(ProducerConfig.RETRIES_CONFIG, 3);
     props.put(ProducerConfig.REQUEST_TIMEOUT_MS_CONFIG, 1);
 
+    return new DefaultKafkaProducerFactory<>(props);
+  }
+
+  private ProducerFactory<String, Employee> producerFactory_AvroSerializerAndDeserializer() {
+    final Map<String, Object> props = ImmutableMap
+        .of(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, KAFKA_BROKERS
+            , ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class
+            , ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class
+            , "schema.registry.url", "http://localhost:8081"
+        );
     return new DefaultKafkaProducerFactory<>(props);
   }
 }
--- src/test/java/com/sachin/work/kafka/BaseTest.java
+++ src/test/java/com/sachin/work/kafka/BaseTest.java
@@ -1,5 +1,7 @@
 package com.sachin.work.kafka;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,11 +11,28 @@ import org.springframework.util.Assert;
 @SpringBootTest
 public class BaseTest {
 
+  @BeforeEach
+  public void setUp() {
+    System.out.println("Inside BaseTest#setUp().....");
+  }
+
+
+  @AfterEach
+  public void tearDown() {
+    System.out.println("Inside BaseTest#tearDown()....., Sleep for sometime to give the method time to finish its stuff");
+
+    try {
+      Thread.currentThread().sleep(2 * 1000);
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+  }
+
   @Autowired
   protected ApplicationContext applicationContext;
 
   @Test
-  void contextSuccessfulLoad() {
+  public void contextSuccessfulLoad() {
     Assert.notNull(applicationContext, "Application Context cannot be null");
   }
 
